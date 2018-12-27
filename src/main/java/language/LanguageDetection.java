f49b60ab9b24ff1be67fb402b0e2a40b986dad37
Ptaq <antyplastik@gmail.com>
2018-12-27 17:28:16 +0100

    offline language detection tests pass

--- src/main/java/language/LanguageDetection.java
+++ src/main/java/language/LanguageDetection.java
@@ -28,19 +28,25 @@ public class LanguageDetection {
 
         for (Language language : availableLanguages.getAvailableLanguages()) {
             HashMap<String, Double> referenceLanguageMap = language.getLetterStat();
-
+            HashMap<String, Double> tempMeanSquareMap = new HashMap<>();
             for (Map.Entry<String, Double> entry : freqMapFromText.entrySet()) {
                 String tmpStr = entry.getKey();
+
                 tmpIndex = 0;
                 if (referenceLanguageMap.containsKey(tmpStr)) {
                     double lettValFromRef = referenceLanguageMap.get(tmpStr);
                     double lettValFromText = freqMapFromText.get(tmpStr);
-                    tmpMeanSquaredErr = tmpMeanSquaredErr + Math.pow((lettValFromRef - lettValFromText), 2);
+//                    tmpMeanSquaredErr = tmpMeanSquaredErr + Math.pow((lettValFromRef - lettValFromText), 2);
+                    tempMeanSquareMap.put(entry.getKey(), Math.pow((lettValFromRef - lettValFromText), 2));
 //                    tmpIndex++;
                 }
             }
-//            tmpMeanSquaredErr = tmpMeanSquaredErr / tmpIndex;
+            tmpMeanSquaredErr = tempMeanSquareMap.entrySet().stream()
+                    .mapToDouble(entry-> entry.getValue())
+                    .sum();
+            tmpMeanSquaredErr = tmpMeanSquaredErr / tempMeanSquareMap.size();
             meanSquaredErrForLanguages.put(language.getLanguageLabel(), tmpMeanSquaredErr);
+            tempMeanSquareMap.clear();
         }
 
         tmpIndex = 0;
--- src/test/java/LanguageDetectionTest.java
+++ src/test/java/LanguageDetectionTest.java
@@ -37,20 +37,30 @@ public class LanguageDetectionTest {
 
     public Object[] testStrings() {
         return new Object[]{
-//            new Object[]{"Is checking for key existence in HashMap always necessary?\n" +
-//                    "\n" +
-//                    "I have a HashMap with say a 1000 entries and I am looking at improving the efficiency. " +
-//                    "If the HashMap is being accessed very frequently, then checking for the key existence at every access will lead to a large overhead. " +
-//                    "Instead if the key is not present and hence an exception occurs, I can catch the exception. (when I know that this will happen rarely). " +
-//                    "This will reduce accesses to the HashMap by half.", "English"
-//            },
+            new Object[]{"Is checking for key existence in HashMap always necessary?\n" +
+                    "\n" +
+                    "I have a HashMap with say a 1000 entries and I am looking at improving the efficiency. " +
+                    "If the HashMap is being accessed very frequently, then checking for the key existence at every access will lead to a large overhead. " +
+                    "Instead if the key is not present and hence an exception occurs, I can catch the exception. (when I know that this will happen rarely). " +
+                    "This will reduce accesses to the HashMap by half.", "English"
+            },
             new Object[]{"Wydarzenia przełomu XX i XXI wieku uświadomiły konieczność podjęcia dyskusji o kształtującej się nowej przestrzeni bezpieczeństwa " +
                     "oraz systemach bezpieczeństwa redukujących ryzyko zagrożeń dla bezpieczeństwa życia i mienia obywateli oraz obiektów tworzących infrastrukturę państwa. " +
                     "Takim szczególnym systemom, od których skuteczności zależy bezpieczeństwo ludzi, organizacji i instytucji, systemów technicznych i innych obiektów o znaczącej wartości " +
                     "poświęcona jest ta książka. Autorzy – reprezentujący różne środowiska i różne specjalności – po przedstawieniu podstaw inżynierii systemów bezpieczeństwa " +
                     "oraz istoty modelowania takich systemów omówili: ryzyko w inżynierii systemów bezpieczeństwa, bezpieczeństwo cybernetyczne, inżynierię systemów bezpieczeństwa informacyjnego, " +
                     "zarządzanie bezpieczeństwem informacji w urzędach administracji publicznej, inżynierię systemów bezpieczeństwa pracy, inżynierię systemów bezpieczeństwa w ruchu drogowym, " +
-                    "zarządzanie bezpieczeństwem zdrowotnym, bezpieczeństwo systemów logistycznych, krajowy i międzynarodowy system zarządzania kryzysowego.", "Polish"}
+                    "zarządzanie bezpieczeństwem zdrowotnym, bezpieczeństwo systemów logistycznych, krajowy i międzynarodowy system zarządzania kryzysowego.", "Polish"},
+                new Object[]{"Kun Muumipeikko, Pikkumyy\n" +
+                        "ja Muumipappa esiintyy.\n" +
+                        "Silloin laulu kutsukoon\n" +
+                        "tuijottelutuokioon.", "Finnish"},
+                new Object[]{
+                        "Ó, guð vors lands! Ó, lands vors guð!\n" +
+                                "Vér lofum þitt heilaga, heilaga nafn!\n" +
+                                "Úr sólkerfum himnanna hnýta þér krans\n" +
+                                "þínir herskarar, tímanna safn.", "Icelandic"
+                }
         };
     }
 
